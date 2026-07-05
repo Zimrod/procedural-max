606e2b11c690c656b6b61446e29ea220c0a7c87b
zimrod <tumaikr@gmail.com>
2026-07-05 14:26:55 +0200

    modified: backend/src/server.ts 3x

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -40,3 +40,10 @@ try {
   console.error(err);
   process.exit(1);
 }
+
+const address = await app.listen({
+  host: "0.0.0.0",
+  port: Number(process.env.PORT) || 3001,
+});
+
+console.log("Listening at: ", address);
