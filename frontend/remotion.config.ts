df862edf1415c78f3a94c2499ffa7b8d7f3700da
zimrod <tumaikr@gmail.com>
2026-07-14 20:38:41 +0200

    modified: frontend/remotion.config.ts

--- frontend/remotion.config.ts
+++ frontend/remotion.config.ts
@@ -6,7 +6,7 @@
 import { Config } from "@remotion/cli/config";
 
 // 🚀 Explicitly tell Remotion where your root orchestration bundle lives
-Config.setEntryPoint("./src/graphics/index.ts");
+Config.setEntryPoint("./src/index.ts");
 
 Config.setVideoImageFormat("jpeg");
 
